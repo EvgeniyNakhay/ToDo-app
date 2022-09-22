85f295b0dc7df8eaa1ebfba9ea117ff1ca223055
Evgeny Nakhay <qwert57894@gmail.com>
2022-09-22 21:08:30 +0300

    changed reducer

--- src/redux/redusers/todosReducer.js
+++ src/redux/redusers/todosReducer.js
@@ -1,13 +1,13 @@
 import { SET_TODO } from '../actions/actionTypes';
 const initialValue = [];
 
-// export default function todosReducer(state = initialValue, action) {
-//     switch (action.type) {
-//         case SET_TODO: {
-//             return [...state , action.payload];
-//         }
-//         default: {
-//             return state;
-//         } 
-//     }
-// }
+export default function todosReducer(state = initialValue, action) {
+    switch (action.type) {
+        case SET_TODO: {
+            return [...state , action.payload];
+        }
+        default: {
+            return state;
+        } 
+    }
+}
